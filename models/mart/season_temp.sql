e9a065cf2db10bbe9453fee6cbdecb9ffe773c75
Lau2601 <l.acevedo2601@gmail.com>
2024-01-18 15:55:00 +0100

    j

--- models/mart/season_temp.sql
+++ models/mart/season_temp.sql
@@ -1,9 +1,9 @@
 WITH add_season AS (
     SELECT *,
             CASE
-                WHEN EXTRACT(MONTH FROM date) IN (3,4,5) THEN 'Spring'
-                WHEN EXTRACT(MONTH FROM date) IN (6,7,8) THEN 'Summer'
-                WHEN EXTRACT(MONTH FROM date) IN (9,10,11) THEN 'Fall'
+                WHEN month_num IN (3,4,5) THEN 'Spring'
+                WHEN month_num IN (6,7,8) THEN 'Summer'
+                WHEN month_num IN (9,10,11) THEN 'Fall'
                 ELSE 'Winter'
             END AS season
     FROM {{ref('prep_temp')}}           
